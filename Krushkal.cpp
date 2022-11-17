a545e3d4241d433638d5e731aff1485b65619623
Rayhan Hasan Labib <87138791+Rayhan-labib7@users.noreply.github.com>
2022-11-17 15:01:01 +0600

    Update Krushkal.cpp

--- Krushkal.cpp
+++ Krushkal.cpp
@@ -124,3 +124,112 @@ void dijkstra()
     
 }
 ...............
+    
+                                               
+
+#include <bits/stdc++.h>
+
+#define     ll                                 long long int   
+
+#define     ll_in1(a)                          cin>>a
+
+#define     ll_in2(a,b)                        cin>>a>>b
+
+#define     ll_in3(a,b,c)                      cin>>a>>b>>c
+
+#define     take_vector_int(x,o,n)             for(int i=o;i<=n;i++){cin>>x[i];}    
+
+#define     srt(v)                             sort(v.begin(),v.end());
+
+#define     rsrt(v)                            reverse(v.begin(),v.end())
+
+#define     gcd(a, b)                          __gcd(a, b)
+
+#define     vi                                 vector<int>
+
+#define     vll                                vector<ll>
+
+#define     endl                               '\n'
+
+#define     yes                                cout<<"YES"<<endl
+
+#define     no                                 cout<<"NO"<<endl
+
+#define mem(a,i)            memset(a, i, sizeof(a))
+
+#define     Labib                              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define mod   1000003                 
+using namespace std;
+const int N=1e5+10;
+const ll inf=1e18;
+vector<pair<int,int>>g[N];
+int parent[N],n;
+ll dijkstra(int source)
+{
+    vector<ll>vis(N,0);
+    vector<ll>dist(N,inf);
+    set<pair<ll,ll>>st;
+    st.insert({0,source});
+    dist[source]=0;
+
+    while (st.size()>0)
+    {
+        auto node=*st.begin();
+        ll  v=node.second;
+        ll  dst=node.first;
+        st.erase(st.begin());
+        for(auto child:g[v])
+        {
+            ll child_v=child.first;
+            ll wt=child.second;
+            if(wt+dist[v]<dist[child_v])
+            {
+                dist[child_v]=wt+dist[v];
+                st.insert({dist[child_v],child_v});
+                parent[child_v]=v;
+            }
+        }
+    }
+    return dist[n];
+    
+}
+
+
+int main()
+{
+   Labib
+   ll m;
+   cin>>n>>m;
+   for(int i=0;i<m;i++)
+   {
+    ll u,v,w;
+    cin>>u>>v>>w;
+    g[u].push_back({v,w});
+    g[v].push_back({u,w});
+   }
+   ll ans=dijkstra(1);
+   if(ans==inf)
+   {
+    cout<<-1<<endl;
+    return 0;
+   }
+   else
+   {
+       vector<int>path;
+       ll x=n;
+       while (x!=1)
+       {
+        path.push_back(x);
+        x=parent[x];
+       }
+       path.push_back(1);
+       for(int i=path.size()-1;i>=0;i--)
+       {
+         cout<<path[i]<<" ";
+       }
+       cout<<endl;
+   }
+
+    return 0;
+}
+.............
