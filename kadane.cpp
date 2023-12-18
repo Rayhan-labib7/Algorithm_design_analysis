124d7ebdf446cbde3c52e4726bb391ece5a92a26
Rayhan Labib <rayhanlabib7@gmail.com>
2023-12-19 00:39:06 +0600

    kadane

---
+++ kadane.cpp
@@ -0,0 +1,15 @@
+int maxSubArray(vector<int>& nums) 
+{
+   int curr_sum=0,best_sum=INT_MIN;
+   for(int i=0;i<nums.size();i++)
+   {
+     curr_sum+=nums[i];
+     if(nums[i]>curr_sum){
+        curr_sum=nums[i];
+     }
+     if(curr_sum>best_sum){
+        best_sum=curr_sum;
+     }
+   }
+   return best_sum;
+}
